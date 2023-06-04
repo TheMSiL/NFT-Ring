f71f728adcc74504b5a0433edc4cdb315fbec083
TheMSiL <silka.maksim15@gmail.com>
2023-06-04 21:20:55 +0300

    add adaptive 450px+

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+//slider//
+
 const firstSlide = document.getElementById('first-slide');
 const lastSlide = document.getElementById('last-slide');
 
@@ -13,3 +15,33 @@ nextBtn.addEventListener('click', () => {
 	firstSlide.style.marginLeft = '0';
 	lastSlide.style.marginRight = '915px';
 });
+
+//slider//
+
+//burger//
+
+const burger = document.getElementById('burger');
+const span1 = document.getElementById('burger-first');
+const span2 = document.getElementById('burger-second');
+const span3 = document.getElementById('burger-third');
+const menu = document.getElementById('burger-menu');
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('clicked');
+
+	if (burger.classList.contains('clicked')) {
+		menu.style.display = 'flex';
+		span2.style.display = 'none';
+		span1.style.transform = 'rotate(45deg)';
+		span1.style.marginBottom = '-5px';
+		span3.style.transform = 'rotate(-45deg)';
+	} else {
+		menu.style.display = 'none';
+		span2.style.display = 'block';
+		span1.style.transform = 'rotate(0deg)';
+		span1.style.marginBottom = '0';
+		span3.style.transform = 'rotate(0deg)';
+	}
+});
+
+//burger//
